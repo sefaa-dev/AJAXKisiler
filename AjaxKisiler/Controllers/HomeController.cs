ae46f1cf6051f394804c179f631c201d8716772e
sefaa-dev <sefayilmaz582@gmail.com>
2022-03-01 12:03:41 +0300

    Add project files.

---
+++ AjaxKisiler/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using AjaxKisiler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxKisiler.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult KisiListele(int id)
+        {
+            List<Kisiler> kisiler = new List<Kisiler>
+            {
+                new Kisiler {Id=1,Isim="Tolga",Sehir="İstanbul"},
+                new Kisiler {Id=2,Isim="Hayatı Kodlamak",Sehir="Youtube"},
+                new Kisiler {Id=3,Isim="X",Sehir="Y"},
+            };
+            Kisiler kisi = kisiler.FirstOrDefault(x => x.Id == id);
+            return Json(kisi, JsonRequestBehavior.AllowGet);
+        
+        }
+    }
+}
---
+++ AjaxKisiler/Models/Kisiler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxKisiler.Models
+{
+    public class Kisiler
+    {
+        public int Id { get; set; }
+        public string Isim { get; set; }
+        public string Sehir { get; set; }
+    }
+}
